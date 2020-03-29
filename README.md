e3d87cac7b6f5b419f4f06e707670934738fb0d3
pjaudiomv <pjaudiomv@gmail.com>
2020-03-29 10:58:36 -0400

    multiple example

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # JFT Slack
 
 Sends the Just For Today to a slack channel.
+
+Ex.
+
+```php
+$sendJft->sendJftToSlack('GHSEJFNLWEASFDwefdsbhjkMv7xB9cTjkfnsvJKFWD', 'just-for-today'); # MVANA
+$sendJft->sendJftToSlack('JHGVJDShjbdSFDwefdsbhjkhkfbsvRJFSJNEADalqp', 'just-for-today'); # CRNA
+$sendJft->sendJftToSlack('EROSFVJNSDJNDDwefdsbhjkMvwefWJFNJNWFNJAEqw', 'just-for-today'); # BMLT
+```
+
+This takes two arguments first a slack incoming webhook api token and second the channel to which we want to send to 
+you can send to as many workspaces and channels as you want.
--- jft-slack.php
+++ jft-slack.php
@@ -118,7 +118,10 @@ class Jft
 }
 
 $sendJft = new Jft;
-$sendJft->sendJftToSlack('GHSEJFNLWEASFDwefdsbhjkMv7xB9cTUMiD6QDytel', 'just-for-today');
+$sendJft->sendJftToSlack('GHSEJFNLWEASFDwefdsbhjkMv7xB9cTjkfnsvJKFWD', 'just-for-today'); # MVANA
+$sendJft->sendJftToSlack('JHGVJDShjbdSFDwefdsbhjkhkfbsvRJFSJNEADalqp', 'just-for-today'); # CRNA
+$sendJft->sendJftToSlack('EROSFVJNSDJNDDwefdsbhjkMvwefWJFNJNWFNJAEqw', 'just-for-today'); # BMLT
 /*
  * This takes two arguments first a slack incoming webhook api token and second the channel to which we want to send to
+ * you can send to as many workspaces and channels as you want.
  */
